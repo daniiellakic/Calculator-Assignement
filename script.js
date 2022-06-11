ec6ccfdcfb28e09193a22b1ecf5916902a6db317
Danijel Lakic <50274258+daniiellakic@users.noreply.github.com>
2022-06-11 17:03:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+// VARIABLES
+let firstNum = null;
+let secondNum = null;
+let operation = null;
+
+// HTML TAGS
+const calcView = document.querySelector("#calc-view");
+const nums = document.querySelectorAll(".num");
+const funcs = document.querySelectorAll(".func");
+const view = document.querySelector('#calc-view p');
+const calcs = document.querySelectorAll(".calc");
+
+// FUNCTIONS THAT CALCULATES
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return b !== 0 ? a / b : "Yikes";
+}
+
+const operate = function(a, func, b) {
+    return func(a, b);
+}
+
+function updateDisplayString(letter) {
+    displayString += letter;
+    calcView.textContent = displayString;
+}
+
+
+
+const addNumber = function() {
+    if (view.textContent.includes(".") & this.textContent === ".") {
+        // DO NOTHING
+    } else {
+        let oneChar = view.offsetWidth / view.textContent.length;
+        if (!oneChar) {
+            oneChar = 0;
+        }
+        if (calcView.offsetWidth - view.offsetWidth - 20 > oneChar) {
+            if (view.textContent === "0") {
+                view.textContent = this.textContent;
+            } else {
+                view.textContent += this.textContent;
+            }
+            console.log(view.offsetWidth)
+        }
+    }
+};
+
+const returnOperation = function(sign) {
+    console.log(sign === "+")
+
+    if (sign === "+") {
+        return add;
+    }
+    if (sign === "-") {
+        return subtract;
+    }
+    if (sign === "/") {
+        return divide;
+    }
+    if (sign === "x") {
+        return multiply;
+    }
+}
+
+// FUNCTION THAT HANDELS CALCULATION
+const handleCalc = function() {
+    console.log("calcs")
+    if (firstNum === null & operation === null) {
+        firstNum = +view.textContent;
+        operation = returnOperation(this.textContent);
+        // console.log(operation);
+        view.textContent = "";
+    }
+}
+
+const handleFuncs = function() {
+    console.log("funcs")
+
+    switch (this.textContent) {
+        case "<":
+            view.textContent = view.textContent.slice(0, view.textContent.length - 1)
+            break;
+        case "Clear":
+            firstNum = null;
+            secondNum = null;
+            operation = null;
+            view.textContent = 0;
+            break;
+        case "+/-":
+            if (view.textContent[0] === "-") {
+                view.textContent = view.textContent.slice(1, view.textContent.length);
+            } else {
+                view.textContent = "-" + view.textContent;
+            }
+            break;
+        case "=":
+            secondNum = +view.textContent;
+            view.textContent = operate(firstNum, operation, secondNum);
+            let i = 0;
+            while (view.offsetWidth > calcView.offsetWidth - 50) {
+                view.textContent = view.textContent.slice(0, view.textContent.length - 1);
+                i++;
+            }
+            if (i > 0) {
+                i += 2;
+                view.textContent = view.textContent.slice(0, view.textContent.length - 2) + "e" + i;
+            }
+            firstNum = null;
+            secondNum = null;
+            operation = null;
+            break;
+    }
+};
+
+
+// LOGIC
+calcs.forEach(element => {
+    element.addEventListener("click", handleCalc);
+});
+
+nums.forEach(element => {
+    element.addEventListener("click", addNumber);
+})
+
+funcs.forEach(element => {
+    element.addEventListener("click", handleFuncs);
+});
